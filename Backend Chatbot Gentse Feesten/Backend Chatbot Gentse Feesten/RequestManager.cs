68d79c06bd9732a52c7640d4c29438c957ec8f52
rvervust <ruben.vervust@telenet.be>
2017-07-03 12:24:57 +0200

    Added comments

--- Backend Chatbot Gentse Feesten/Backend Chatbot Gentse Feesten/RequestManager.cs
+++ Backend Chatbot Gentse Feesten/Backend Chatbot Gentse Feesten/RequestManager.cs
@@ -10,6 +10,11 @@ using System.Runtime.Serialization.Json;
 
 namespace Backend_Chatbot_Gentse_Feesten
 {
+
+    /// <summary>
+    /// Generic rest client, T schould be a valid model class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class RequestManager<T>
     {
 
@@ -19,7 +24,11 @@ namespace Backend_Chatbot_Gentse_Feesten
                         
         }
 
-
+        /// <summary>
+        /// Fetches data at url and returns it as a defined model class
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public T getResultAsObject(String url)
         {
             return ProcessRepositories(url).Result;
@@ -27,12 +36,14 @@ namespace Backend_Chatbot_Gentse_Feesten
 
         private static async Task<T> ProcessRepositories(String url)
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            var serializer = new DataContractJsonSerializer(typeof(List<Event>));
-            var streamTask = client.GetStreamAsync(url);
-            var events = serializer.ReadObject(await streamTask);
-            return (T) events;
+           
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                var serializer = new DataContractJsonSerializer(typeof(List<Event>));
+                var streamTask = client.GetStreamAsync(url);
+                var events = serializer.ReadObject(await streamTask);
+                return (T)events;
+            
         }
 
     }
