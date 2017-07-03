a29db9c778191548f5e0b44dc08931d7f1a10804
jawieme <jana.wieme@ugent.be>
2017-07-03 13:06:47 +0200

    package om messages in te vormen

---
+++ Backend Chatbot Gentse Feesten/Backend Chatbot Gentse Feesten/MessageBuilder/MessageEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend Chatbot Gentse Feesten.MessageBuilder
+{
+    public class MessageEvent
+    {
+    }
+}
