d09b4e7f4d10b2250c33f539a267316576fa2691
rvervust <ruben.vervust@telenet.be>
2017-07-03 10:32:52 +0200

    Initial commit

---
+++ Backend Chatbot Gentse Feesten/Backend Chatbot Gentse Feesten/RequestManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+
+namespace Backend_Chatbot_Gentse_Feesten
+{
+    public class RequestManager
+    {
+
+        public RequestManager()
+        {
+            ProcessRepositories().Wait();
+        }
+
+
+        private static async Task ProcessRepositories()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            var stringTask = client.GetStringAsync("https://datatank.stad.gent/4/cultuursportvrijetijd/gentsefeestenlocaties.json");
+
+            var msg = await stringTask;
+            Debug.Write(msg);
+        }
+
+    }
+
+
+}
