eebca1abe9db372d6ec4c8db1b962ecd732b9e2b
JorgWieme <jorg.wieme@ugent.be>
2017-07-03 15:10:10 +0200

    MessengerController updated for repeating

--- ChatbotGF/Backend Chatbot Gentse Feesten/Controllers/MessengerController.cs
+++ ChatbotGF/Backend Chatbot Gentse Feesten/Controllers/MessengerController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using static Chatbot_GF.BotData.MessengerData;
+using System.Net;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 
 namespace Chatbot_GF.Controllers
 {
@@ -24,6 +27,49 @@ namespace Chatbot_GF.Controllers
             return null;
         }
 
-       
+
+        [ActionName("Receive")]
+        [HttpPost]
+        public ActionResult ReceivePost(BotRequest data)
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                foreach (var entry in data.entry)
+                {
+                    foreach (var message in entry.messaging)
+                    {
+                        if (string.IsNullOrWhiteSpace(message?.message?.text))
+                            continue;
+
+                        var msg = "You said: " + message.message.text;
+                        var json = $@" {{recipient: {{  id: {message.sender.id}}},message: {{text: ""{msg}"" }}}}";
+                        await PostRawAsync("https://graph.facebook.com/v2.6/me/messages?access_tokenEAADbmmTTQZBkBAGCYtymjKzMGGTr817rNVgsqNMAFxxVZCkrvKN5dkJfj88rhy3onuVwCAziCWPB1sBl3Jf5C6FujRZC1g6lRaRk1yW0M5EQvSQiKLFtkbNAYSqFpRZAsuBDqUXYpQz2K5PwZCopyzC5skFa1e7LOUhEZAdelk2QZDZD", json);
+                    }
+                }
+            });
+
+            return Ok();
+        }
+
+        private async Task<string> PostRawAsync(string url, string data)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            using (var requestWriter = new StreamWriter(await request.GetRequestStreamAsync()))
+            {
+                requestWriter.Write(data);
+            }
+
+            var response = (HttpWebResponse) await request.GetResponseAsync();
+            if (response == null)
+                throw new InvalidOperationException("GetResponse returns null");
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
     }
 }
