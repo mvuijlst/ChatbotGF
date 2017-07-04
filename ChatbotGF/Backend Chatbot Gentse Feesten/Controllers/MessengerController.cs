7bcbffead2fa1701f657ed54452eb8a31f8e55bd
JorgWieme <jorg.wieme@ugent.be>
2017-07-04 09:54:25 +0200

    JsonBuilder added

--- ChatbotGF/Backend Chatbot Gentse Feesten/Controllers/MessengerController.cs
+++ ChatbotGF/Backend Chatbot Gentse Feesten/Controllers/MessengerController.cs
@@ -40,11 +40,9 @@ namespace Chatbot_GF.Controllers
                     {
                         if (string.IsNullOrWhiteSpace(message?.message?.text))
                             continue;
-
-                        var msg = "You said: " + message.message.text;
-                        var json = $@" {{recipient: {{  id: {message.sender.id}}},message: {{text: ""{msg}"" }}}}";
-                        Console.Write("Message: " + msg + " to id: " + message.sender.id +"\n");
-                        String res = PostRawAsync("https://graph.facebook.com/v2.6/me/messages?access_token=EAADbmmTTQZBkBAGCYtymjKzMGGTr817rNVgsqNMAFxxVZCkrvKN5dkJfj88rhy3onuVwCAziCWPB1sBl3Jf5C6FujRZC1g6lRaRk1yW0M5EQvSQiKLFtkbNAYSqFpRZAsuBDqUXYpQz2K5PwZCopyzC5skFa1e7LOUhEZAdelk2QZDZD", json).Result;
+                       
+                        var json = new JsonBuilder(message.sender.id, "You said: " + message.message.text);
+                        String res = PostRawAsync("https://graph.facebook.com/v2.6/me/messages?access_token=EAADbmmTTQZBkBAGCYtymjKzMGGTr817rNVgsqNMAFxxVZCkrvKN5dkJfj88rhy3onuVwCAziCWPB1sBl3Jf5C6FujRZC1g6lRaRk1yW0M5EQvSQiKLFtkbNAYSqFpRZAsuBDqUXYpQz2K5PwZCopyzC5skFa1e7LOUhEZAdelk2QZDZD", json.Json).Result;
                     }
                 }
             });
---
+++ ChatbotGF/Backend Chatbot Gentse Feesten/MessageBuilder/JsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot_GF.Controllers
+{
+    public class JsonBuilder
+    {
+        private string json;
+        public JsonBuilder(string user_id, string msg)
+        {
+            json = $@" {{recipient: {{  id: {user_id}}},message: {{text: ""{msg}"" }}}}";
+        }
+
+        public string Json {get;}
+    }
+}
