6aa09888b2d7900678a04e555e60955af6a5b62c
jawieme <jana.wieme@ugent.be>
2017-07-04 16:10:06 +0200

    Messenger manager: koppeling messenger en queries

---
+++ ChatbotGF/Backend Chatbot Gentse Feesten/MessengerManager/Manager.cs
@@ -0,0 +1,71 @@
+﻿using Chatbot_GF.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot_GF.MessengerManager
+{
+    public enum SearchState { START, PROGRESS, READYTO}
+
+    public class Manager
+    {
+        public Dictionary<long, User> activeUsers = new Dictionary<long, User>();
+
+        /// <summary>
+        /// Method to be called when user pushes on "get Started" --> makes a new user in the list + saves his id and timestamp
+        /// </summary>
+        /// <param name="id"></param>
+        public void startUser(long id)
+        {
+            activeUsers.Add(id, new User(id));
+            saveUsers(id, DateTime.Now);
+        }
+
+        /// <summary>
+        /// Saves all the users + timestamps for case of emergency + knowing how many users
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="stamp"></param>
+        private void saveUsers(long id, DateTime stamp)
+        {
+            string path = Path.GetTempFileName();
+            string line = "" + id + "\t" + stamp;
+            // This text is a1lways added, making the file longer over time
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("This");
+            }
+
+        }
+
+        /// <summary>
+        /// Method for each event call from messenger
+        /// </summary>
+        /// <param name="id"></param>
+        public void changeUserState(long id, string payload)
+        {
+            User currentUser;   //contains the user object linked to the messengerperson who sends an event
+            if (!activeUsers.ContainsKey(id))
+            {
+                currentUser = new User(id);
+                activeUsers.Add(id, currentUser);
+            }
+            else{
+                currentUser = activeUsers[id];
+            }
+
+
+
+        }
+        
+
+        
+
+
+
+
+
+    }
+}
---
+++ ChatbotGF/Backend Chatbot Gentse Feesten/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using Chatbot_GF.MessengerManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot_GF.Model
+{
+    public class User
+    {
+        public long id { get; set; }                    //Messenger id from user
+        public SearchState state { get; set; }
+        public IList<DateTime> dates { get; set; }
+        public IList<string> locations { get; set; }    //needs to be URL's of the locations
+        public IList<string> keywords { get; set; }
+
+        public User(long messId)
+        {
+            state = SearchState.START;
+            messId = id;
+            dates.Add(DateTime.Now);
+        }
+
+
+    }
+}
