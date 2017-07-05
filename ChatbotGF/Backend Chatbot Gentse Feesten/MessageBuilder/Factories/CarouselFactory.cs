e1d5a3c12c496015f341153d71b716186bd5976a
JorgWieme <jorg.wieme@ugent.be>
2017-07-05 13:53:16 +0200

    Carousel

---
+++ ChatbotGF/Backend Chatbot Gentse Feesten/MessageBuilder/Factories/CarouselFactory.cs
@@ -0,0 +1,21 @@
+﻿using Chatbot_GF.MessageBuilder.Model;
+using Chatbot_GF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot_GF.MessageBuilder.Factories
+{
+    public class CarouselFactory
+    {
+        public static GenericMessage makeCarousel(long id, List<Event> events)
+        {
+            List<Element> elements = new List<Element>();
+            // TODO: lijst init
+            IPayload payload = new PayloadMessage("generic", elements, true, "horizontal");
+            Attachment attachment = new Attachment("template", payload);
+            return new GenericMessage(id, attachment);
+        }
+    }
+}
