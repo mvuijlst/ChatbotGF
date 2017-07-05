eabcd0c457f5fd9e56e9b9745552bea86f4d8807
JorgWieme <jorg.wieme@ugent.be>
2017-07-05 15:20:55 +0200

    Adapted to links CarouselFactory

--- ChatbotGF/Backend Chatbot Gentse Feesten/MessageBuilder/Factories/CarouselFactory.cs
+++ ChatbotGF/Backend Chatbot Gentse Feesten/MessageBuilder/Factories/CarouselFactory.cs
@@ -18,14 +18,22 @@ namespace Chatbot_GF.MessageBuilder.Factories
             List<Element> elements = new List<Element>();
             foreach (var eve in events)
             {
-                // voor aantal knopen if doen wachten op event link 
                 List<IButton> buttons = new List<IButton>();
-                // link aanpassen 
-                DefaultAction defaultAction = new DefaultAction("web_url", "https://gentsefeesten.stad.gent", true);
-                buttons.Add(new ButtonUrl("Meer info", "web_url", "https://gentsefeesten.stad.gent", true));
-                buttons.Add(new ButtonPayload("Kleine uitleg", "postback", "DEVELOPER_DEFINED_DESCRIPTION-" + /*eve.url*/ "https://gentsefeesten.stad.gent"));
-                buttons.Add(new ButtonPayload("Wanneer is het?", "postback", "DEVELOPER_DEFINED_HOURS-" + /*eve.*/ "https://gentsefeesten.stad.gent"));
-                elements.Add(new Element(eve.name.nl, "https://cdn.pastemagazine.com/www/system/images/photo_albums/cuberdons/large/cuberdons-1.jpg?1384968217"/*eve.image*/, eve.organizer, buttons, defaultAction));
+                DefaultAction defaultAction;
+                if (eve.url == null)
+                {
+                    buttons.Add(new ButtonUrl("Meer info", "web_url", "https://gentsefeesten.stad.gent", true));
+                    defaultAction = new DefaultAction("web_url", "https://gentsefeesten.stad.gent", true);
+                }
+                else
+                {
+                    // misschien moet je de url nog whitelisten !!!!!!!!!!!!!!
+                    defaultAction = new DefaultAction("web_url", eve.url, true);
+                    buttons.Add(new ButtonUrl("Meer info", "web_url", eve.url, true));
+                }
+                buttons.Add(new ButtonPayload("Kleine uitleg", "postback", "DEVELOPER_DEFINED_DESCRIPTION-" + eve.url));
+                buttons.Add(new ButtonPayload("Wanneer is het?", "postback", "DEVELOPER_DEFINED_HOURS-" + eve.url));
+                elements.Add(new Element(eve.name.nl, eve.image, eve.organizer, buttons, defaultAction));
             }
             IPayload payload = new PayloadMessage("generic", elements, true, "horizontal");
             Attachment attachment = new Attachment("template", payload);
