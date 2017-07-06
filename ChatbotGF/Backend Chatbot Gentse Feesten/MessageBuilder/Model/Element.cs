8e28c6a383afff961526f52aed385ab1b0ad3569
JorgWieme <jorg.wieme@ugent.be>
2017-07-06 09:32:41 +0200

    Removed subtitles in carousel

--- ChatbotGF/Backend Chatbot Gentse Feesten/MessageBuilder/Model/Element.cs
+++ ChatbotGF/Backend Chatbot Gentse Feesten/MessageBuilder/Model/Element.cs
@@ -7,17 +7,15 @@ namespace Chatbot_GF.MessageBuilder.Model
 {
     public class Element
     {
-        public Element(string title, string image_url, string subtitle, List<IButton> buttons, DefaultAction default_action)
+        public Element(string title, string image_url, List<IButton> buttons, DefaultAction default_action)
         {
             this.title = title; // required
             this.image_url = image_url; // not required
             this.buttons = buttons; // not required
             this.default_action = default_action; // not required
-            this.subtitle = subtitle; // not required
         }
         public string title { get; set; }
         public string image_url { get; set; }
-        public string subtitle { get; set; }
         public DefaultAction default_action { get; set; }
         public List<IButton> buttons { get; set; }
     }
