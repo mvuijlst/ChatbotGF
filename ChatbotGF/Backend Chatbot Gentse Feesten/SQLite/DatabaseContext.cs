4b7cb57be1275ee514306189c6e4e98296d0645d
JorgWieme <jorg.wieme@ugent.be>
2017-07-07 10:27:18 +0200

    Several packages added

---
+++ ChatbotGF/Backend Chatbot Gentse Feesten/SQLite/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chatbot_GF.Model;
+
+namespace Chatbot_GF.SQLite
+{
+
+        public class DatabaseContext : DbContext
+        {
+            public DbSet<User> Posts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=chatbot.db");
+            }
+        }
+ }
